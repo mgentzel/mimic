585dbf8e6176959897ecc8fef2645c80defc35d0
Mark Gentzel <markgentzel@gmail.com>
2016-02-25 09:39:10 -0500

    fixed build and clean scripts to use ignored build dir, etc.

--- iOS/build_mimic_iOS.sh
+++ iOS/build_mimic_iOS.sh
@@ -16,11 +16,18 @@
 ## -d flag is interpreted differently in OSX - needs to be -R, so cp -pd must be changed to cp -pR  
 ## see: http://stackoverflow.com/questions/23001775/error-installing-flite-on-mac-osx
 
+TOP_DIR=`pwd`
+BUILD_DIR=$TOP_DIR/build
+echo BUILD_DIR $BUILD_DIR
+
 # clean previous builds
+cd $BUILD_DIR
+echo Working directory ${PWD}
 rm -rf install_*
 rm -rf output-*
+cd $TOP_DIR
+echo Working directory ${PWD}
 
-TOP_DIR=`pwd`
 
 # up to /mimic dir
 cd ../
@@ -30,38 +37,39 @@ echo Working directory ${PWD}
 # build armv64  -arch arm64
 echo Building arch arm64
 
-./configure --prefix=$TOP_DIR/install_arm64 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch arm64" --host=arm
+./configure --prefix=$BUILD_DIR/install_arm64 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch arm64" --host=arm
 make clean && make && make install
 
 # build armv7
 echo Building arch armv7
 
-./configure --prefix=$TOP_DIR/install_armv7 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7" --host=arm
+./configure --prefix=$BUILD_DIR/install_armv7 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7" --host=arm
 make clean && make && make install
 
 # build armv7s
 # buiilding for armv7s may not be absolutely required - recent developer forum thread suggest that armv7 runs fine on this arch, but not well documented
 echo Building arch armv7s
 
-./configure --prefix=$TOP_DIR/install_armv7s CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7s" --host=arm
+./configure --prefix=$BUILD_DIR/install_armv7s CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphoneos clang -arch armv7s" --host=arm
 make clean && make && make install
 
 # build simulator i386 and x86_64
 echo Building arch i386
 
-./configure --prefix=$TOP_DIR/install_i386 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch i386"
+./configure --prefix=$BUILD_DIR/install_i386 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch i386"
 make clean && make && make install
 
 echo Building arch x86_64
 
-./configure --prefix=$TOP_DIR/install_x86_64 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch x86_64"
+./configure --prefix=$BUILD_DIR/install_x86_64 CFLAGS="-Ofast -mios-version-min=5.0" LDFLAGS="-flto" CC="xcrun -sdk iphonesimulator clang -arch x86_64"
 make clean && make && make install
 
 # build universal
 echo Building Universal Static Lib
 
-# back to working dir
-cd $TOP_DIR
+# back to build dir
+#cd $TOP_DIR
+cd $BUILD_DIR
 echo Working directory ${PWD}
 
 # dir for lipo output - universal binary
@@ -70,6 +78,5 @@ mkdir -p output-ios
 xcrun lipo -create $(find install_*/lib -name "libmimic.a") -o output-ios/libmimic.a
 # copy inlcude (headers)
 cp -r install_armv7/include output-ios/.
-
 
 
